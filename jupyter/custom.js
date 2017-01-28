8bf66eaa440ffe5c8c0881ade5154973ca3bad0a
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2017-01-28 15:17:21 +0100

    custom.js code kicks in

--- jupyter/custom.js
+++ jupyter/custom.js
@@ -1,10 +1,9 @@
-// a first seed for styling a notebook inside a mooc infra
-function mooc_style(Jupyter) {
+define([
+  'base/js/namespace',
+  'base/js/events'
+], function(IPython, events) {
+   events.on('app_initialized.NotebookApp', function() {
     $("div#header-container").hide();
-    $("body.notebook_app").css("background", "red");
-    
-}
-
-events.on("notebook_loaded.Notebook", function () {
-    mooc_sytle(Juyter);
-})
+    $("body.notebook_app").css("border-width", "2px");
+   });
+});
