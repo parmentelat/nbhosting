9c78964d87f301f433ed493952accd31f2f9c77e
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2017-01-30 14:34:22 +0100

    a first rough layout for embedded notebooks

--- jupyter/custom.js
+++ jupyter/custom.js
@@ -1,10 +1,12 @@
 define([
     'base/js/namespace',
     'base/js/events'
-], function(IPython, events) {
+], function(Jupyter, events) {
+
+    //////////////////////////////////////////////////
     events.on('app_initialized.NotebookApp', function() {
 
-	console.log("custom.js: messing with menubar/toolbar");
+	console.log("custom.js for mooc embedding: entering app_initialized");
 	// not truly useful, just a test indeed
 	// this is because the menubar does not exactyl stand out
 	
@@ -12,8 +14,8 @@ define([
 	$("div#header-container").hide();
 	
 	//// second horizontal area (menubar)
-	// top right (python2/python3..)
-	$("p#kernel_indicator").hide();
+	// hide menu entries
+
 	// 'Edit'
 	$("div#menubar>div>div>div>ul.nav>li:nth-child(2)").hide();
 	// View
@@ -31,12 +33,28 @@ define([
 	$("#maintoolbar>div>div>div:nth-child(7)").hide();
 	// celltoolbar
 	$("#maintoolbar>div>div>div:nth-child(8)").hide();
+
+	// top right (python2/python3..)
+	//$("p#kernel_indicator").hide();
+	var last_button_group = $("div#maintoolbar-container>div:last-child");
+	var ids_to_move = ['kernel_indicator', 'readonly-indicator',
+			   'modal_indicator', 'notification_area'];
+	for (i in ids_to_move) {
+	    var id=ids_to_move[i];
+	    last_button_group.after($("#"+id));
+	}
+	    
+	
+
+	//////////
+	console.log("custom.js for mooc embedding: exiting app_initialized");
     });
 
     // set this aside - from benjamin's code
     // it's kind of working but too intrusive
     // this html needs to be injected, not to replace 
     var update_metadata = function() {
+	console.log("showing notebook metadata");
 	var notebook = Jupyter.notebook;
 	var notebookmeta = "";
 	if(notebook.metadata.notebookname)  
@@ -46,19 +64,35 @@ define([
 	var notebookmetadiv = '<div class="navbar-nobg"><div class="container"><div id="ipython_notebook" class="nav brand pull-left">'+notebookmeta+'</div></div></div>' ;
 	$('div#header').html(notebookmetadiv);
     }
+
+    var inactivate_non_code_cells = function(Jupyter) {
+	console.log("custom.js : inactivating non-code cells");
+	var cells = Jupyter.notebook.get_cells();
+	for(var i in cells){
+	    var cell = cells[i];
+	    if (!(cell instanceof IPython.CodeCell)) {
+		cell.code_mirror.setOption('readOnly', 'nocursor'); 
+	    }
+	}
+	$('.text_cell').unbind('dblclick');
+    }
+
+    // this might sound like a good idea but needs more checking
+    var redefine_enter_in_command_mode = function(Jupyter) {
+	console.log("redefining the Enter key in command mode");
+	Jupyter.keyboard_manager.command_shortcuts.add_shortcut(
+	    'Enter', "jupyter-notebook:run-cell-and-select-next")
+    }
     
+    //////////////////////////////////////////////////
     events.on('notebook_loaded.Notebook', function(){
-	console.log("showing notebook metadata");
-	$("#maintoolbar>div>div>div:nth-child(7)").hide();
-//	$('div#menubar-container').show();
-//	var cells = IPython.notebook.get_cells();
-//	for (var i in cells){
-//	    var cell = cells[i];
-//	    if (!(cell instanceof IPython.CodeCell)) {
-//		cell.code_mirror.setOption('readOnly','nocursor'); 
-//	    }
-//	}
-//	$('.text_cell').unbind('dblclick');
+	console.log("custom.js for mooc embedding: entering notebook_loaded");
+	$('body.notebook_app').show();
+	inactivate_non_code_cells(Jupyter);
+//	redefine_enter_in_command_mode(Jupyter);
+	
+	//////////
+	console.log("custom.js for mooc embedding: exiting notebook_loaded");
     });
   
 });
