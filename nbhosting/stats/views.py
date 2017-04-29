891e03b991f06fd583948a6861348b5e185d7457
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2017-04-29 15:10:28 +0200

    relooked the stats page

--- nbhosting/stats/views.py
+++ nbhosting/stats/views.py
@@ -13,9 +13,44 @@ from nbhosting.stats.stats import Stats
 @csrf_protect
 def show_stats(request, course):
 
-    return render(request, "stats.html", {
-        'course': course,
-    })
+    section1 = {
+        'title' : 'Progression',
+        'id' : 'PROGRESSION',
+        'subsections' : [
+            { 'plotly_name' : 'stats-students',
+              'title' : 'Students - who showed up at least once'},
+            { 'plotly_name' : 'stats-notebooks',
+              'title' : 'Notebooks - read at least once'},
+        ]
+    }
+    section2 = {
+        'title' : 'Activity',
+        'id' : 'ACTIVITY',
+        'subsections' : [
+            { 'plotly_name' : 'stats-jupyters',
+              'title' : 'Jupyter containers'},
+            { 'plotly_name' : 'stats-kernels',
+              'title' : 'Running kernels'},
+            { 'plotly_name' : 'stats-student-counts',
+              'title' : 'Students with a homedir'},
+        ]
+    }
+    section3 = {
+        'title' : 'System',
+        'id' : 'SYSTEM',
+        'subsections' : [
+            { 'plotly_name' : 'stats-ds-percent',
+              'title' : 'Disk Space Usage %'},
+            { 'plotly_name' : 'stats-ds-free',
+              'title' : 'Free Space'},
+            { 'plotly_name' : 'stats-cpu-load',
+              'title' : 'CPU loads'},
+        ]
+    }
+    env = { 'course' : course,
+            'sections' : [section1, section2, section3] }
+
+    return render(request, "stats.html", env)
 
 @csrf_protect
 def send_daily_metrics(request, course):
