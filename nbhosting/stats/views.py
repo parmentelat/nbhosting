c4eff8acacec0f0c6d69609788afb6f3dd220c91
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2017-11-28 16:23:42 +0100

    close #47

--- nbhosting/stats/views.py
+++ nbhosting/stats/views.py
@@ -13,49 +13,67 @@ from nbhosting.stats.stats import Stats
 @csrf_protect
 def show_stats(request, course):
 
-    section1 = {
-        'title' : 'Progression',
-        'id' : 'PROGRESSION',
-        'subsections' : [
-            { 'plotly_name' : 'plotly-students',
-              'title' : 'Students - who showed up at least once'},
+    sections = []
+    sections.append(dict(
+        title = 'Progression',
+        id = 'PROGRESSION',
+        subsections = [
             { 'plotly_name' : 'plotly-nbstudents-per-notebook',
-              'title' : 'Students per notebook'},
+              'title' : 'Students per notebook',
+#              'hide' : True,
+            },
             { 'plotly_name' : 'plotly-nbstudents-per-nbnotebooks',
-              'title' : 'Number of notebooks per student'},
+              'title' : 'Number of notebooks per student',
+#              'hide' : True,
+            },
+            { 'plotly_name' : 'plotly-students',
+              'title' : 'Students - who showed up at least once',
+              'hide' : True,
+            },
             { 'plotly_name' : 'plotly-notebooks',
               'title' : 'Notebooks - read at least once',
-              'hide' : True},
+#              'hide' : True,
+            },
+        ]
+    ))
+
+    sections.append(dict(
+        title = 'Details',
+        id = 'DETAILS',
+        subsections = [
             { 'plotly_name' : 'plotly-heatmap',
               'title' : 'Complete map',
-              'hide' : True},
-            
+              'hide' : True,
+            },
         ]
-    }
-    section2 = {
-        'title' : 'Activity',
-        'id' : 'ACTIVITY',
-        'subsections' : [
-            { 'plotly_name' : 'plotly-containers-kernels',
-              'title' : 'Jupyter containers and kernels'},
-            # xxx student_homes as collected in counts
-            # is not really useful so we don't show it anymore
-        ]
-    }
-    section3 = {
-        'title' : 'System',
-        'id' : 'SYSTEM',
-        'subsections' : [
-            { 'plotly_name' : 'plotly-ds-percent',
-              'title' : 'Free Disk Space %'},
-            { 'plotly_name' : 'plotly-ds-free',
-              'title' : 'Free Disk Space in Bytes'},
-            { 'plotly_name' : 'plotly-cpu-load',
-              'title' : 'CPU loads'},
-        ]
-    }
+    ))
 
-    sections = [section1, section2, section3]
+    sections.append(dict(
+        title = 'Activity',
+        id = 'ACTIVITY',
+        subsections = [
+            { 'plotly_name' : 'plotly-containers-kernels',
+              'title' : 'Jupyter containers and kernels',
+             'hide' : True,
+            },
+        ]
+    ))
+    sections.append(dict(
+        title = 'System',
+        id = 'SYSTEM',
+        subsections = [
+            { 'plotly_name' : 'plotly-ds-percent',
+              'title' : 'Free Disk Space %',
+            },
+            { 'plotly_name' : 'plotly-ds-free',
+              'title' : 'Free Disk Space in Bytes',
+            },
+            { 'plotly_name' : 'plotly-cpu-load',
+              'title' : 'CPU loads',
+            },
+        ]
+    ))
+
     # set 'hide' to False by default
     for section in sections:
         for subsection in section['subsections']:
@@ -68,22 +86,26 @@ def show_stats(request, course):
 @csrf_protect
 def send_daily_metrics(request, course):
     stats = Stats(course)
-    metrics = stats.daily_metrics()
-    result = json.dumps(metrics)
-    return HttpResponse(result, content_type = "application/json")
+    encoded = json.dumps(stats.daily_metrics())
+    return HttpResponse(encoded, content_type = "application/json")
 
 
 @csrf_protect
 def send_monitor_counts(request, course):
     stats = Stats(course)
-    counts = stats.monitor_counts()
-    result = json.dumps(counts)
-    return HttpResponse(result, content_type = "application/json")
+    encoded = json.dumps(stats.monitor_counts())
+    return HttpResponse(encoded, content_type = "application/json")
 
 
 @csrf_protect
 def send_material_usage(request, course):
     stats = Stats(course)
-    usage = stats.material_usage()
-    result = json.dumps(usage)
-    return HttpResponse(result, content_type = "application/json")
+    encoded = json.dumps(stats.material_usage())
+    return HttpResponse(encoded, content_type = "application/json")
+
+@csrf_protect
+def send_animated_attendance(request, course):
+    stats = Stats(course)
+    encoded = json.dumps(stats.animated_attendance())
+    return HttpResponse(encoded, content_type = "application/json")
+    
