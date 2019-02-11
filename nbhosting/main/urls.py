12d1e3e1b64bff635270146b4d6a0ac740d2d714
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2019-02-11 11:19:29 +0100

    fix major bug in urls management need to explicitly mention $ at the end of the regexp so to catch all parts of the URL, as opposed to being happy with less greedy matches

--- nbhosting/main/urls.py
+++ nbhosting/main/urls.py
@@ -18,42 +18,42 @@ import nbhosting.main.views
 urlpatterns = [
     # tweaking greedy and non greedy so that the .ipynb suffix
     # will go away if there's one or even two
-    re_path(r'ipythonExercice/(?P<course>[\w_.-]+)/'
-         r'(?P<notebook>[-\w_\+/\.]+?)(.ipynb){0,2}/(?P<student>[\w_.-]+)',
+    re_path(r'^ipythonExercice/(?P<course>[\w_.-]+)/'
+         r'(?P<notebook>[-\w_\+/\.]+?)(.ipynb){0,2}/(?P<student>[\w_.-]+)$',
                         nbhosting.edxfront.views.edx_request
     ),
-    re_path(r'ipythonShare/(?P<course>[\w_.-]+)/'
-         r'(?P<notebook>[-\w_\+/\.]+?)(.ipynb){0,2}/(?P<student>[\w_.-]+)',
+    re_path(r'^ipythonShare/(?P<course>[\w_.-]+)/'
+         r'(?P<notebook>[-\w_\+/\.]+?)(.ipynb){0,2}/(?P<student>[\w_.-]+)$',
                         nbhosting.edxfront.views.share_notebook
     ),
 
     # regular users who log in
-    re_path(r'^auditor/courses.*',
+    re_path(r'^auditor/courses.*$',
                         nbhosting.courses.views.auditor_list_courses),
-    re_path(r'^auditor/course/(?P<course>[\w_.-]+)',
+    re_path(r'^auditor/course/(?P<course>[\w_.-]+)$',
                         nbhosting.courses.views.auditor_show_course),
     re_path(r'^auditor.*',
                         nbhosting.main.views.welcome),
 
 
     # super user
-    re_path(r'staff/courses/update-from-git/(?P<course>[\w_.-]+)',
+    re_path(r'staff/courses/update-from-git/(?P<course>[\w_.-]+)$',
                         nbhosting.courses.views.update_from_git),
-    re_path(r'staff/courses/build-image/(?P<course>[\w_.-]+)',
+    re_path(r'staff/courses/build-image/(?P<course>[\w_.-]+)$',
                         nbhosting.courses.views.build_image),
-    re_path(r'staff/courses/clear-staff/(?P<course>[\w_.-]+)',
+    re_path(r'staff/courses/clear-staff/(?P<course>[\w_.-]+)$',
                         nbhosting.courses.views.clear_staff),
     re_path(r'staff/courses/.*',
                         nbhosting.courses.views.staff_list_courses),
-    re_path(r'staff/course/(?P<course>[\w_.-]+)',
+    re_path(r'staff/course/(?P<course>[\w_.-]+)$',
                         nbhosting.courses.views.staff_show_course),
-    re_path(r'staff/stats/daily_metrics/(?P<course>[\w_.-]+)',
+    re_path(r'staff/stats/daily_metrics/(?P<course>[\w_.-]+)$',
                         nbhosting.stats.views.send_daily_metrics),
-    re_path(r'staff/stats/monitor_counts/(?P<course>[\w_.-]+)',
+    re_path(r'staff/stats/monitor_counts/(?P<course>[\w_.-]+)$',
                         nbhosting.stats.views.send_monitor_counts),
-    re_path(r'staff/stats/material_usage/(?P<course>[\w_.-]+)',
+    re_path(r'staff/stats/material_usage/(?P<course>[\w_.-]+)$',
                         nbhosting.stats.views.send_material_usage),
-    re_path(r'staff/stats/(?P<course>[\w_.-]+)',
+    re_path(r'staff/stats/(?P<course>[\w_.-]+)$',
                         nbhosting.stats.views.show_stats),
     re_path(r'staff.*',
                         nbhosting.main.views.welcome),
