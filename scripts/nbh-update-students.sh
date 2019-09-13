9f271470962abbb29bca0437c0f6b8fc553618fa
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2019-09-13 09:45:24 +0200

    nbh-update-students can do on multiple courses and find them by pattern like nbh-manage courses-list

--- scripts/nbh-update-students.sh
+++ scripts/nbh-update-students.sh
@@ -1,73 +1,100 @@
 #!/bin/bash
 
+
+PULL_MODE=""
+SILENT_MODE=""
+FIND_MODE=""
+
+
+# print current commit's hahs on stdout
+function current-hash() {
+    git log -1 --pretty='%h'
+}
+ 
+
+function update-course() {
+    course=$1; shift
+
+    echo "========== git pull for $course"
+    if [ -z "$SILENT_MODE" ]; then
+        nbh-manage course-pull-from-git $course 
+    else
+        nbh-manage course-pull-from-git $course >& /dev/null
+    fi
+
+    cd /nbhosting/current/courses-git/$course
+    expected_hash=$(current-hash)
+    echo "expected hash extracted from $(pwd) as $expected_hash"
+
+
+    for student_home in /nbhosting/current/students/*.*; do
+        student=$(basename $student_home)
+        student_course=$student_home/$course
+        if [ ! -d $student_home/$course ]; then
+#	echo ==== $student has no working space for course $course
+	    continue
+        fi
+        cd $student_course
+        student_hash=$(current-hash)
+        if [ $student_hash == "$expected_hash" ]; then
+            [ -z "$SILENT_MODE" ] && echo == $student OK
+        else
+	    if [ -z "$PULL_MODE" ]; then
+	        echo XX $student is on $student_hash
+	    else
+	        sudo -u $student git pull
+	        new_hash=$(current-hash)
+	        if [ "$new_hash" == "$expected_hash" ]; then
+		    [ -z "$SILENT_MODE" ] && echo "++ $student pulled OK"
+	        else
+		    echo "-- $student still behind on $new_hash"
+	        fi
+	    fi
+        fi
+    done
+}
+
+
 function usage() {
-    echo "Usage $0 [-p] [-s] course"
+    echo "Usage $0 [-p] [-s] [-f] course"
     echo "  performs a git pull on that course"
     echo "  then picks corresponding hash"
     echo "  and checks all the students spaces in students/*.*"
     echo "options"
     echo " -p: actually pulls from the students spaces"
     echo " -s: silent mode; students that are OK are not reported"
+    echo " -f: courses are found by pattern using nbh-manage courses-list"
     exit 1
 }
 
-function current_hash() {
-    git log -1 --pretty='%h'
-}
- 
-# default is to just be watching
-pull_mode=""
-silent_mode=""
-while getopts "ps" option; do
-    case $option in
-            p) pull_mode="true" ;;
-            s) silent_mode="true" ;;
+
+function main() {
+
+    [[ -n "$@" ]] || usage
+
+    # default is to just be watching
+    while getopts "fps" option; do
+        case $option in
+            p) PULL_MODE="true" ;;
+            s) SILENT_MODE="true" ;;
+            f) FIND_MODE="true" ;;
 	    ?) usage ;;
-    esac
-done
-shift $((OPTIND-1))
-# reset OPTIND for subsequent calls to getopts
-OPTIND=1
-    
-course=$1; shift
+        esac
+    done
+    shift $((OPTIND-1))
+    # reset OPTIND for subsequent calls to getopts
+    OPTIND=1
 
-[ -n "$course" ] || usage
-[[ -n "$@" ]] && usage
-
-echo "pulling from git for course $course"
-if [ -z "$silent_mode" ]; then
-    nbh-manage course-pull-from-git $course 
-else
-    nbh-manage course-pull-from-git $course >& /dev/null
-fi
-
-cd /nbhosting/current/courses-git/$course
-expected_hash=$(current_hash)
-echo "expected hash extracted from $(pwd) as $expected_hash"
-
-
-for student_home in /nbhosting/current/students/*.*; do
-    student=$(basename $student_home)
-    student_course=$student_home/$course
-    if [ ! -d $student_home/$course ]; then
-#	echo ==== $student has no working space for course $course
-	continue
-    fi
-    cd $student_course
-    student_hash=$(current_hash)
-    if [ $student_hash == "$expected_hash" ]; then
-        [ -z "$silent_mode" ] && echo == $student OK
+    [[ -z "$@" ]] && usage
+    if [ -n "$FIND_MODE" ]; then
+        courses=$(nbh-manage courses-list "$@")
     else
-	if [ -z "$pull_mode" ]; then
-	    echo XX $student is on $student_hash
-	else
-	    sudo -u $student git pull
-	    new_hash=$(current_hash)
-	    if [ "$new_hash" == "$expected_hash" ]; then
-		[ -z "$silent_mode" ] && echo "++ $student pulled OK"
-	    else
-		echo "-- $student still behind on $new_hash"
-	    fi
-	fi
+        courses="$@"
     fi
-done
+    for course in $courses; do
+        update-course $course
+    done
+}
+
+main "$@"    
+
