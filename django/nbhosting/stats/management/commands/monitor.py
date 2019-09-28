3c6cd7490eb48d8298afb6e77343f528bb537b04
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2019-09-28 09:38:45 +0200

    tweak monitor launcher: * set default cycle to 10 minutes instead of 15 * show default values in help message

--- django/nbhosting/stats/management/commands/monitor.py
+++ django/nbhosting/stats/management/commands/monitor.py
@@ -6,24 +6,29 @@ from django.core.management.base import BaseCommand
 
 from nbhosting.stats.monitor import Monitor
 
-class Command(BaseCommand):
+DEFAULT_PERIOD = 10
+DEFAULT_IDLE = 30
 
+class Command(BaseCommand):
 
     def add_arguments(self, parser):
         # default was 2 hours with the jupyter-v4 strategy,
         # which was based on the last time a notebook was opened
         # with jupyter-v5 we can now really apply a timeout
         parser.add_argument(
-            "-p", "--period", default=15, type=int,
-            help="monitor period in minutes - how often are checks performed")
+            "-p", "--period", default=DEFAULT_PERIOD, type=int,
+            help="monitor period in minutes - how often are checks performed "
+                 f"(default={DEFAULT_PERIOD})")
         parser.add_argument(
-            "-i", "--idle", default=30, type=int, dest='idle',
-            help="timeout in minutes - kill containers idle more than that")
+            "-i", "--idle", default=DEFAULT_IDLE, type=int, dest='idle',
+            help="timeout in minutes - kill containers idle more than that "
+                 f"(default={DEFAULT_IDLE})")
         parser.add_argument(
             "-u", "--unused", default=15, type=int, dest='unused',
             help="timeout in days - remove containers unused more than that")
         parser.add_argument(
             "-d", "--debug", action='store_true', default=False)
+
 
     def handle(self, *args, **kwargs):
         monitor = Monitor(
