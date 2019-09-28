7b80c0816150bf72b2d9b3d6db3907856599e287
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2019-09-28 10:20:08 +0200

    no semantic change, but less verbose output when container is unreachable

--- django/nbhosting/stats/monitor.py
+++ django/nbhosting/stats/monitor.py
@@ -189,10 +189,10 @@ class MonitoredJupyter:
         # this somehow tends to happen a lot sometimes
         # until we figure it out, let's make it less conspicuous
         except ClientConnectionError as exc:
-            logger.exception(f"{self.name}, url={url}, {type(exc)}: {exc}")
+            logger.info(f"{self} could not be reached for last activity, url={url}, {type(exc)}: {exc}")
 
         except Exception:
-            logger.exception(f"Cannot probe number of kernels with {self}")
+            logger.exception(f"Cannot probe number of kernels with {self} - unhandled exception")
 
 
     def remove_container(self):
