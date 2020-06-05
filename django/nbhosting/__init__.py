b7cbbdedc02a7c2369bdae4468156a1cca601897
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2020-06-05 09:21:38 +0200

    typo

--- django/nbhosting/__init__.py
+++ django/nbhosting/__init__.py
@@ -1,10 +1,9 @@
 # ever since Ive been using db-backed models
 # I keep on running into the infamous
 # "AppRegistryNotReady: Apps aren't loaded yet"
-# exception thrown by django ar random stages
+# exception thrown by django at random stages
 # including and most frustatingly when running
 # setup.py and/or manage.py
 #
 # so I am taking the drastic step of removing *ANY* autoimport
 # feature in here
-
