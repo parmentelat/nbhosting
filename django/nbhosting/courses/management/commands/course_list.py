3b3588a5c0a305ab5cb0f96609acdf5e84e832cf
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2020-12-12 15:44:17 +0100

    close #118

--- django/nbhosting/courses/management/commands/course_list.py
+++ django/nbhosting/courses/management/commands/course_list.py
@@ -25,7 +25,7 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument("patterns", nargs='*', type=str)
         parser.add_argument(
-            "-l", "--list", action="count", default=0, dest="verbose",
+            "-l", "--list", action="count", default=0,
             help=("Give more output. Option is additive, and can be used up to 2 "
                   "times."),
         )
@@ -33,44 +33,66 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
 
         patterns = kwargs['patterns']
-        verbose = kwargs['verbose']
+        list_flag = kwargs['list']
 
-        def show_course(cd, max_name, max_image):
+        def groups(cd):
+            return " + ".join(group.name for group in cd.registered_groups.all())
+
+        def show_course(cd, max_name, max_image, max_groups):
+            col_name = f"{max_name+1}s"
+            col_groups = f"{max_groups+1}s"
             autopull = "[AP]" if cd.autopull else ""
             archived = "[AR]" if cd.archived else ""
-            col1 = f"{max_name+1}s"
-            col2 = f"{max_image+1}s"
-            hash = f"{cd.current_hash():9s}"
-            line = f"{cd.coursename:{col1}}"
-            if verbose == 1:
-                line += f"{cd.image:{col2}}"
-                line += f"{autopull:5s}"
-                line += f"{hash}"
-                line += f"{cd.giturl}"
-            else:
-                groups = cd.registered_groups.all()
-                groupnames = " + ".join(group.name for group in groups)
-                line += f"{cd.image:{col2}}"
-                line += f"{autopull:5s}"
-                line += f"{archived:5s}"
-                line += f"{hash}"
-                line += f"RGS=[{groupnames}]"
-                line += f"{cd.giturl}"
-            print(line)
-                
+            flags = "".join([x for x in (autopull, archived) if x])
+            flags = f"{flags:9s}"
+            hash_part = f"{cd.current_hash():8s}"
+            groups_part = f"{groups(cd):{col_groups}}"
+            image = cd.image
+
+            line = f"{cd.coursename:{col_name}}"
+            if list_flag == 0:
+                return line
+
+            image_exists = None
+            if list_flag >= 3:
+                import podman
+                podman_url = "unix://localhost/run/podman/podman.sock"
+                with podman.ApiConnection(podman_url) as podman_api:
+                    image_exists = podman.images.image_exists(podman_api, cd.image)
+                warning = "!" if not image_exists else " "
+                image = f"{warning}{image}{warning}"
+                # we may have 2 more characters in the image part
+                max_image += 2
+            col_image = f"{max_image+1}s"
+
+            image_part = f"{image:{col_image}}"
+            line += image_part
+            line += flags
+            if list_flag == 1:
+                return line
+            line += hash_part
+            line += groups_part
+            line += f"{cd.giturl}"
+            if image_exists is False:
+                escape = chr(27)
+                line = f"{escape}[1m{escape}[31m{line}{escape}[0m"
+            return line
+
 
         all_coursedirs = sorted(
             CourseDir.objects.all(),
             key=lambda coursedir: coursedir.coursename)
-        max_name = max((len(cd.coursename) for cd in all_coursedirs), default=4)
-        max_image = max((len(cd.image) for cd in all_coursedirs), default=4)
-        for coursedir in all_coursedirs:
-            name = coursedir.coursename
-            if not patterns:
-                show_course(coursedir, max_name, max_image)
-            else:
-                for pattern in patterns:
-                    if pattern == '*' or pattern in name:
-                        show_course(coursedir, max_name, max_image)
-                        break
+        if not patterns:
+            selected = all_coursedirs
+        else:
+            def is_matched(cd):
+                return any((pattern == '*' or pattern in cd.coursename)
+                           for pattern in patterns)
+            selected = [cd for cd in all_coursedirs if is_matched(cd)]
+
+        max_name = max((len(cd.coursename) for cd in selected), default=4)
+        max_image = max((len(cd.image) for cd in selected), default=4)
+        max_groups = max((len(groups(cd)) for cd in selected), default=3)
+        for coursedir in selected:
+            print(show_course(coursedir, max_name, max_image, max_groups))
         return 0
