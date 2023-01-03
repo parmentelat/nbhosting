7e54d546f6be4c6940a550e9c440717842d516cf
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2023-01-03 13:50:23 +0100

    nbh-to-jb-toc.py accepts a -p option so one can specify replacements to do on paths - typically removing the notebooks/ part

--- scripts/nbh-to-jb-toc.py
+++ scripts/nbh-to-jb-toc.py
@@ -11,12 +11,18 @@ def strip(filename):
     """
     return str(Path(filename).with_suffix(""))
 
-def inject_track_into_toc(track: dict, toc: dict) -> dict:
+def inject_track_into_toc(track: dict, toc: dict, path_to_replace) -> dict:
+    def cleansed(notebook):
+        if path_to_replace is None:
+            return notebook
+        else:
+            old, new = path_to_replace.split('->')
+            return notebook.replace(old, new)
     def chapter(notebook):
         if '*' in notebook:
-            return {'glob': strip(notebook)}
+            return {'glob': strip(cleansed(notebook))}
         else:
-            return {'file': strip(notebook)}
+            return {'file': strip(cleansed(notebook))}
     def chapters(notebooks):
         return [chapter(notebook) for notebook in notebooks]
     new_parts = [dict(
@@ -26,12 +32,19 @@ def inject_track_into_toc(track: dict, toc: dict) -> dict:
     toc['parts'] = new_parts
     return toc
 
-def update_jb_from_nbh(jupyter_book_yaml, nbhosting_yaml, trackname, dry_run):
+def update_jb_from_nbh(jupyter_book_yaml, nbhosting_yaml, trackname, path_to_replace, dry_run):
     try:
-        with (open(jupyter_book_yaml) as jb,
-              open(nbhosting_yaml) as nbh):
+        with open(nbhosting_yaml) as nbh:
             nbh_data = yaml.load(nbh, yaml.FullLoader)
-            jb_data = yaml.load(jb, yaml.FullLoader)
+
+        try:
+            with open(jupyter_book_yaml) as jb:
+                jb_data = yaml.load(jb, yaml.FullLoader)
+                if not jb_data:
+                    raise ValueError("empty jb toc")
+        except (FileNotFoundError, ValueError):
+            print("it is unsafe to start from an empty {jupyter_book_yaml}")
+            return False
 
         tracks = nbh_data['tracks']
         if trackname is None:
@@ -39,7 +52,7 @@ def update_jb_from_nbh(jupyter_book_yaml, nbhosting_yaml, trackname, dry_run):
         else:
             track = next(track for track in tracks if track['id'] == trackname)
 
-        inject_track_into_toc(track, jb_data)
+        inject_track_into_toc(track, jb_data, path_to_replace)
 
         if dry_run:
             jupyter_book_yaml += ".dry"
@@ -56,6 +69,10 @@ def main():
     parser.add_argument("-t", "--track", default=None, help="nbhosting track id")
     parser.add_argument("-n", "--dry-run", default=False, action='store_true',
                         help="append .dry to the written filename")
+    parser.add_argument("-p", "--path-replace", default=None, dest='path_to_replace',
+                        help="provide a string replacement specification in the form "
+                             "old-string->new-string; useful to deal with paths when running"
+                             "jupyter-book not from the toplevel")
     parser.add_argument("nbhosting_yaml")
     parser.add_argument("jupyter_book_yaml")
     args = parser.parse_args()
@@ -63,6 +80,7 @@ def main():
     result = update_jb_from_nbh(args.jupyter_book_yaml,
                                 args.nbhosting_yaml,
                                 args.track,
+                                args.path_to_replace,
                                 args.dry_run)
     if result:
         print(f"(over)wrote {result}")
