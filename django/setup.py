2078c397993a543cbc2b81ffb13360ecbd974de0
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2023-12-10 15:04:46 +0100

    setup.py: please the linter

--- django/setup.py
+++ django/setup.py
@@ -10,20 +10,23 @@ from .version import __version__
 try:
     with open("README.md") as readme:
         long_description = readme.read()
-except Exception as e:
+except Exception as exc:
     long_description = "no description could be found"
 
 setup(
-    name = 'nbhosting',
-    version = __version__,
-    author = "Thierry Parmentelat",
-    author_email = "thierry.parmentelat@inria.fr",
-    packages = find_packages(),
-    url = "https://github.com/parmentelat/nbhosting",
-    license = "See LICENSE",
-    description = "An infrastructure for hosting notebooks behind an open-edX MOOC platform, or for registered students like in a classroom",
-    long_description = long_description,
-    install_requires = [
+    name='nbhosting',
+    version=__version__,
+    author="Thierry Parmentelat",
+    author_email="thierry.parmentelat@inria.fr",
+    packages=find_packages(),
+    url="https://github.com/parmentelat/nbhosting",
+    license="See LICENSE",
+    description=(
+        "An infrastructure for hosting notebooks behind an open-edX"
+        " MOOC platform, or for registered students like in a classroom"
+    ),
+    long_description=long_description,
+    install_requires=[
         'pip',
         'setuptools',
         'wheel',
@@ -37,13 +40,13 @@ setup(
         'rich',        # this is for podman, should not be needed, see also
                        # https://github.com/containers/podman/issues/20964
         'redis',
-        'nbstripout', # for course-pull
+        'nbstripout',  # for course-pull
     ],
-    setup_requires = [],
-    tests_require = [],
+    setup_requires=[],
+    tests_require=[],
 
     # see https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers = [
+    classifiers=[
         'Development Status :: 3 - Alpha',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
