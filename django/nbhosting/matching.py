e627f7e611bdba4dd37968da53770710fbccc4f2
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2024-04-17 14:58:46 +0200

    fixes \ in regexps that need a raw-string

--- django/nbhosting/matching.py
+++ django/nbhosting/matching.py
@@ -1,24 +1,24 @@
 import re
 
 def matching_policy(name, patterns):
-    """
+    r"""
     matching policy for course or students is as follows
 
     patterns is an iterable of patterns
     matching means a 'OR' of all patterns
     if patterns is empty, name is a match
-    
+
     if one pattern is empty, name is a match
-    if one pattern starts with a =, name matches 
+    if one pattern starts with a =, name matches
         only if it is an exact match of what is after =
-    if one pattern does not contain a '*', 
+    if one pattern does not contain a '*',
         all names that **contain** the filter match
     if one pattern contains a '*', name needs to match that pattern
         in the re sense, with:
         '*' replaced with '.*'
         '.' replaced by \.
     """
-    
+
     if not patterns:
         return True
     for pattern in patterns:
@@ -29,10 +29,10 @@ def matching_policy(name, patterns):
         if pattern[0] == '=':
             if name == pattern[1:]:
                 return True
-        # if '*' is 
+        # if '*' is
         elif '*' in pattern:
             pattern = (pattern
-                .replace('.', '\.')
+                .replace('.', r'\.')
                 .replace('*', '.*'))
             pattern = f"^{pattern}$"
             if re.match(pattern, name):
