cd956ed4404fee7e9baf9393e78d827de4d7e3a4
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2024-04-18 10:49:17 +0200

    improve js-side reading of incoming chunks
    tries to read all lines instead of bailing out if one is broken
    displays inline material when non-json lines are found
    
    still not perfect but workable
    what's wrong at this point:
    * most importantly, the random chunking done by the django synchronous view
    * we could try to join lines when this non-json thing happens
      hopefully this won't be necessary if we gain control of the streams

--- django/assets/js/process.js
+++ django/assets/js/process.js
@@ -9,7 +9,7 @@ async function read_process_stream(command, elementId) {
         headers: {
             'Content-Type': 'application/json',
             'X-CSRFToken': csrftoken,
-          },
+        },
     }
     const url = '/process/run'
     const response = await fetch(url, request)
@@ -17,37 +17,42 @@ async function read_process_stream(command, elementId) {
     const reader = response.body.getReader()
     const decoder = new TextDecoder()
 
+    const addLine = (line, className) => {
+        const newdiv = document.createElement('div')
+        newdiv.innerText = line
+        newdiv.classList.add(className)
+        element.appendChild(newdiv)
+    }
     while (true) {
         const { value, done } = await reader.read()
         if (done)
             break
-        try {
-            // decode bytes as text
-            const chunk = decoder.decode(value, { stream: true })
-            // console.log('chunk:', chunk)
-            for (const line of chunk.split("\n")) {
-                if (! line)
-                    continue
+        // decode bytes as text
+        const chunk = decoder.decode(value, { stream: true })
+        console.debug(`chunk: ${chunk.length} long`, chunk)
+        for (const line of chunk.split("\n")) {
+            if (!line)
+                continue
+            try {
                 const data = JSON.parse(line)
                 // console.log('.')
                 if ((data.type === 'stdout') || (data.type === 'stderr')) {
-                    const newdiv = document.createElement('div')
-                    newdiv.innerText = data.text
-                    newdiv.classList.add(data.type)
-                    element.appendChild(newdiv)
+                    addLine(data.text, data.type)
                 } else {
                     const retcod = data.retcod
-                    const newdiv = document.createElement('div')
-                    newdiv.classList.add("retcod")
                     const message = (retcod === 0) ? "Success (retcod=0)" : `Failed with retcode=${retcod}`
-                    newdiv.innerText = message
-                    element.appendChild(newdiv)
+                    addLine(message, "retcod")
                     const classname = (retcod === 0) ? 'success' : 'failure'
                     element.classList.add(classname)
                 }
+            } catch (e) {
+                console.log("error while decoding json:", "error=", e)
+                addLine(`probably truncated json line ${line}`, "non-json")
             }
-        } catch (e) {
-            console.log("error while decoding json:", "error=", e)
         }
+    }
+    console.log('done')
+    if (!element.classList.contains('failure') && !element.classList.contains('success')) {
+        element.classList.add('unknown')
     }
 }
