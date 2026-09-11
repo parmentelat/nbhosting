028b175fc77200945a86feab54a9c887a494b22a
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2026-09-11 11:16:40 +0200

    a little more verbose backup script

--- zz-devel/backup-prod-and-dev-from.sh
+++ zz-devel/backup-prod-and-dev-from.sh
@@ -5,6 +5,7 @@ sources="/nbhosting/prod /nbhosting/dev"
 function local_backup() {
     for source in $sources; do
         local dest=$source.$(hostname -s)
+        echo "$(date) - backing up $source to $dest"
         rsync -a --delete $source/ $dest/
     done
 }
@@ -14,14 +15,15 @@ function remote_backup() {
     for source in $sources; do
         local remote=$(cut -d. -f1 <<< $hostname)
         local dest=$source.$remote
+        echo "$(date) - backing up $hostname:$source to $dest"
         rsync -a --delete $hostname:$source/ $dest
     done
 }
 
 hostname="$1"; shift
 
-if [ -z "$hostname" ]; then
-    local_backup
-else
+if [ -n "$hostname" ]; then
     remote_backup $hostname
+else
+    local_backup
 fi
