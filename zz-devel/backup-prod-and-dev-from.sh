695d9c976ceb859bed53eb8079ffa2b072b8658c
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2026-09-11 11:25:45 +0200

    the backup script supports -i and -n for rsync

--- zz-devel/backup-prod-and-dev-from.sh
+++ zz-devel/backup-prod-and-dev-from.sh
@@ -6,7 +6,7 @@ function local_backup() {
     for source in $sources; do
         local dest=$source.$(hostname -s)
         echo "$(date) - backing up $source to $dest"
-        rsync -a --delete $source/ $dest/
+        rsync -a --delete $rsync_opts $source/ $dest/
     done
 }
 
@@ -16,9 +16,26 @@ function remote_backup() {
         local remote=$(cut -d. -f1 <<< $hostname)
         local dest=$source.$remote
         echo "$(date) - backing up $hostname:$source to $dest"
-        rsync -a --delete $hostname:$source/ $dest
+        rsync -a --delete $rsync_opts $hostname:$source/ $dest
     done
 }
+
+function usage() {
+    echo "Usage: $(basename $0) [-i] [-n] [hostname]"
+    echo "  -i    pass -i (itemize-changes) to rsync"
+    echo "  -n    pass -n (dry-run) to rsync"
+    exit 1
+}
+
+rsync_opts=""
+while getopts "in" opt; do
+    case $opt in
+        i) rsync_opts="$rsync_opts -i" ;;
+        n) rsync_opts="$rsync_opts -n" ;;
+        *) usage ;;
+    esac
+done
+shift $((OPTIND - 1))
 
 hostname="$1"; shift
 
